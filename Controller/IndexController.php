ee7984faf2e28aa0dd921e1def817cee4d28b5af
Michael Zapf <m.zapf@mztx.de>
2017-12-26 12:53:03 +0100

    replace base_url dynamically

--- Controller/IndexController.php
+++ Controller/IndexController.php
@@ -2,21 +2,22 @@
 
 namespace mztx\ShinagePlayerBundle\Controller;
 
-use mztx\ShinagePlayerBundle\Service\GuidCreator;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 class IndexController extends Controller
 {
 
-    public function indexAction()
+    public function indexAction(Request $request)
     {
-        /** @var GuidCreator $guidCreator */
-        #$guidCreator = $this->get('shinage.player.guid_creator');
-        #var_dump($guidCreator); exit;
-
         $playerHtml = __DIR__ . '/../Resources/public/player.html';
         $appContent = file_get_contents($playerHtml);
+        $appContent = str_replace(
+            '%%base_url%%',
+            $request->getSchemeAndHttpHost() . $request->getBaseUrl(),
+            $appContent
+        );
         return new Response($appContent);
     }
 }
